78cdbd41b203058d88dc24ff588e9452b5c4ca57
Ryan Sauge <ryan.sauge@protonmail.com>
2022-11-10 18:22:39 +0100

    Add test for the Access Control

--- test/RuleEngine/RuleEngine.t.sol
+++ test/RuleEngine/RuleEngine.t.sol
@@ -47,6 +47,7 @@ contract RuleEngineTest is Test, HelperContract, RuleWhitelist {
         assertEq(resUint256, 2);
     }
 
+
     function testCanClearRules() public{
         // Arrange
         vm.prank(WHITELIST_OPERATOR_ADDRESS);
