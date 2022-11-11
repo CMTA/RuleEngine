2b44313d7ee7b6e0605d2f82fa31b79d68b75315
Ryan Sauge <ryan.sauge@protonmail.com>
2022-11-11 10:14:12 +0100

    Add test remove non existant rule

--- test/RuleEngine/RuleEngine.t.sol
+++ test/RuleEngine/RuleEngine.t.sol
@@ -90,6 +90,20 @@ contract RuleEngineTest is Test, HelperContract, RuleWhitelist {
         assertEq(resUint256, 2);
     }
 
+    function testCanRemoveNonExistantRule() public{
+        // Arrange
+        vm.prank(WHITELIST_OPERATOR_ADDRESS);
+        RuleWhitelist ruleWhitelist1 = new RuleWhitelist();
+        
+        // Act
+        vm.prank(RULE_ENGINE_OPERATOR_ADDRESS);
+        ruleEngineMock.removeRule(ruleWhitelist1);
+
+        // Assert
+        resUint256 = ruleEngineMock.ruleLength(); 
+        assertEq(resUint256, 1);
+    }
+
     function testCanRemoveLatestRule() public{
         // Arrange
         vm.prank(WHITELIST_OPERATOR_ADDRESS);
@@ -128,8 +142,6 @@ contract RuleEngineTest is Test, HelperContract, RuleWhitelist {
  
         resUint256 = ruleEngineMock.ruleLength(); 
         assertEq(resUint256, 2);
-
-
     }
 
     function testRuleLength() public {
