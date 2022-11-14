c3ff847e4fe2ad88a578c1a501a23bcdc36d7095
Ryan Sauge <ryan.sauge@protonmail.com>
2022-11-14 15:58:59 +0100

    Separate AccessControl between RuleWhitelist and RuleEngine

--- test/HelperContract.sol
+++ test/HelperContract.sol
@@ -15,13 +15,15 @@ abstract contract HelperContract {
     address constant ADDRESS2 = address(6);
     address constant ADDRESS3 = address(7);
     string constant RULE_ENGINE_ROLE_HASH =
-        "0xacd8e63c87cd69425697967e0428694fc91f0f3cf09ccac63f65401cfa956c94";
+        "0x774b3c5f4a8b37a7da21d72b7f2429e4a6d49c4de0ac5f2b831a1a539d0f0fd2";
     string constant WHITELIST_ROLE_HASH =
-        "0xd5fd42827a296da4efc38d4948bb7299b7d1683149226f9a69c0d6f3b7d621ee";
+        "0xdc72ed553f2544c34465af23b847953efeb813428162d767f9ba5f4013be6760";
     string constant DEFAULT_ADMIN_ROLE_HASH =
         "0x0000000000000000000000000000000000000000000000000000000000000000";
     RuleWhitelist ruleWhitelist;
     CMTAT CMTAT_CONTRACT;
-
+    bytes32 public constant RULE_ENGINE_ROLE =
+        keccak256("RULE_ENGINE_ROLE");
+    
     constructor() {}
 }
