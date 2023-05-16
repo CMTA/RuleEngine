4ea192a275fe2bfeaf308d33b9c3bfc1c2c017ab
Ryan Sauge <ryan.sauge@protonmail.com>
2023-05-16 16:22:10 +0200

    Remove useless comment lines

--- test/RuleEngine/AccessControl/RuleEngineAccessControlOZ.t.sol
+++ test/RuleEngine/AccessControl/RuleEngineAccessControlOZ.t.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.17;
 
 import "forge-std/Test.sol";
-//import "CMTAT/CMTAT.sol";
 import "../../../lib/openzeppelin-contracts/contracts/access/AccessControl.sol";
 import "../../HelperContract.sol";
 import "src/RuleEngine.sol";
--- test/RuleEngine/RuleEngine.t.sol
+++ test/RuleEngine/RuleEngine.t.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.17;
 
 import "forge-std/Test.sol";
-//import "CMTAT/CMTAT.sol";
 import "../HelperContract.sol";
 import "src/RuleEngine.sol";
 
