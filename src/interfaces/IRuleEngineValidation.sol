4dca5d2f84590b23821cc220b04cb1da26b000f5
Ryan Sauge <ryan.sauge@protonmail.com>
2024-06-05 10:37:05 +0200

    create two interfaces for RuleEngineValidation

--- src/interfaces/IRuleEngineValidation.sol
+++ src/interfaces/IRuleEngineValidation.sol
@@ -3,25 +3,6 @@
 pragma solidity ^0.8.20;
 
 interface IRuleEngineValidation {
-    /**
-     * @dev define the rules, the precedent rules will be overwritten
-     */
-    function setRulesValidation(address[] calldata rules_) external;
-
-    /**
-     * @dev return the number of rules
-     */
-    function rulesCountValidation() external view returns (uint256);
-
-    /**
-     * @dev return the rule at the index specified by ruleId
-     */
-    function ruleValidation(uint256 ruleId) external view returns (address);
-
-    /**
-     * @dev return all the rules
-     */
-    function rulesValidation() external view returns (address[] memory);
 
     /**
      * @dev See ERC-1404
@@ -41,4 +22,27 @@ interface IRuleEngineValidation {
         address _to,
         uint256 _amount
     ) external view returns (bool isValid);
+}
+
+
+interface IRuleEngineValidationCommon {
+    /**
+     * @dev define the rules, the precedent rules will be overwritten
+     */
+    function setRulesValidation(address[] calldata rules_) external;
+
+    /**
+     * @dev return the number of rules
+     */
+    function rulesCountValidation() external view returns (uint256);
+
+    /**
+     * @dev return the rule at the index specified by ruleId
+     */
+    function ruleValidation(uint256 ruleId) external view returns (address);
+
+    /**
+     * @dev return all the rules
+     */
+    function rulesValidation() external view returns (address[] memory);
 }
